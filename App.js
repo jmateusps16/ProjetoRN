74bfb6333760f3e51bb4ed4780efaebda32aede8
Mateus Palmeira <jmpsf@cin.ufpe.br>
2023-05-20 12:00:54 -0300

    inicio

---
+++ App.js
@@ -0,0 +1,44 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TouchableOpacity } from 'react-native';
+import IndexScreen from './src/screens/IndexScreen';
+import ShowFeedScreen from './src/screens/ShowFeedScreen';
+import AddFeedScreen from './src/screens/AddFeedScreen';
+import { Feather } from '@expo/vector-icons';
+import { Provider as FeedListProvider } from './src/context/FeedListContext';
+import { Provider as FeedProvider } from './src/context/FeedContext';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen 
+        name="Index" 
+        component={IndexScreen} 
+        options={({navigation}) => ({
+          headerRight: () => (
+            <TouchableOpacity onPress={() => console.log('implementar')}>
+              <Feather name="plus" size={30} />
+            </TouchableOpacity>
+          )
+        })
+        }
+      />
+      <Stack.Screen name="Show" component={ShowFeedScreen} />
+      <Stack.Screen name="Add" component={AddFeedScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default () => {
+  return (
+    <FeedListProvider>
+      <FeedProvider>
+        <App />
+      </FeedProvider>
+    </FeedListProvider>
+  );
+};
